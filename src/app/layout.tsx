602409977a36a1717a74eed011e36d1d0c624030
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2025-06-03 11:48:29 -0300

    fix: google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Roboto_Slab } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const robotoSlab = Roboto_Slab({
-  subsets: ["latin"],
-  variable: "--font-roboto-slab",
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -32,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${robotoSlab.variable} antialiased`}
-      >
+      <body className={`${robotoSlab.className} antialiased`}>
         <main className="flex flex-col max-w-screen-sm mx-auto min-h-screen p-2 md:p-4 pt-18 md:pt-24 md:border-x border-white bg-white">
           <Navbar />
           {children}
